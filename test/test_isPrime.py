a1b9fdbf70b03901b190a931f0a78332e8893cb3
sparsha-bindu <85055472+sparsha-bindu@users.noreply.github.com>
2023-03-16 11:59:00 +0530

    Update test_isPrime.py

--- test/test_isPrime.py
+++ test/test_isPrime.py
@@ -1,1 +1,17 @@
-wwwww
+import unittest
+from isPrime import is_prime
+
+class TestIsPrime(unittest.TestCase):
+
+    def test_is_prime(self):
+        self.assertTrue(is_prime(5))
+        self.assertFalse(is_prime(1))
+        self.assertTrue(is_prime(2))
+        self.assertTrue(is_prime(3))
+        self.assertFalse(is_prime(4))
+        self.assertTrue(is_prime(5))
+        self.assertFalse(is_prime(6))
+        self.assertTrue(is_prime(7))
+        self.assertFalse(is_prime(8))
+        self.assertFalse(is_prime(9))
+        self.assertFalse(is_prime(10)
